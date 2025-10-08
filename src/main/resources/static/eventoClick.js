76250639d917099a637b8e14600825f5a268a3e6
Guilherme Santini <santini.guilherme.gs@gmail.com>
2025-10-07 21:38:20 -0300

    remover elementos

--- src/main/resources/static/eventoClick.js
+++ src/main/resources/static/eventoClick.js
@@ -1,30 +1,33 @@
 document.getElementById("bt-fabricantes").addEventListener("click", async function(event) {
     setMostrarOcultarElemento(true, ".section");
-    removeTabelaRepetida("table-fabricante");
+    // removeTabelaRepetida("table-fabricante");
+    setRemoverElementos(".table-dados");
     document.querySelector("#fabricantes").style.display = "block";
     const dados = await getData("http://localhost:8080/api/fabricantes");
     if (dados) {
-        document.querySelector("#fabricantes").appendChild(criarTabela(dados, "table-fabricante"));
+        document.querySelector("#fabricantes").appendChild(criarTabela(dados, "Fabricante", "table-dados"));
     }    
 });
 
 document.getElementById("bt-modelos").addEventListener("click", async function(event) {
     setMostrarOcultarElemento(true, ".section");
-    removeTabelaRepetida("table-modelos");
+    // removeTabelaRepetida("table-modelos");
+    setRemoverElementos(".table-dados");
     document.querySelector("#modelos").style.display = "block";
     const dados = await getData("http://localhost:8080/api/modelos");
     if (dados) {
-        document.querySelector("#modelos").appendChild(criarTabela(dados, "table-modelos"));
+        document.querySelector("#modelos").appendChild(criarTabela(dados, "Modelos", "table-dados"));
     }
 });
 
 document.getElementById("bt-veiculos").addEventListener("click", async function(event) {
     setMostrarOcultarElemento(true, ".section");
-    removeTabelaRepetida("table-veiculos");
+    // removeTabelaRepetida("table-veiculos");
+    setRemoverElementos(".table-dados");
     document.querySelector("#veiculos").style.display = "block";
     const dados = await getData("http://localhost:8080/api/veiculos");
     if (dados) {
-        document.querySelector("#veiculos").appendChild(criarTabela(dados, "table-veiculos"));
+        document.querySelector("#veiculos").appendChild(criarTabela(dados, "Veiculos", "table-dados"));
     }
     
 });
--- src/main/resources/static/gerenciarElementos.js
+++ src/main/resources/static/gerenciarElementos.js
@@ -2,19 +2,29 @@
 
 */
 
-const criarTabela = function(dados, nome) {
+const criarTabela = function(dados, titulo = "Tabela", classe) {
     const tabela = document.createElement("table");
-    tabela.className = nome;
     const thead = document.createElement("thead");
     const tbody = document.createElement("tbody");
 
     const cabecalho = Object.keys(dados[0]);
+
+    const trTitle = document.createElement("tr");
+    const th = document.createElement("th");
+    th.textContent = titulo;
+    th.colSpan = cabecalho.length;
+    trTitle.appendChild(th);
+    thead.appendChild(trTitle);
+
     const tr = document.createElement("tr");
     cabecalho.forEach(function(campo) {
         const th = document.createElement("th");
         th.textContent = campo;
         tr.appendChild(th);
     });
+
+    tabela.classList.add(classe);
+
     thead.appendChild(tr);
     tabela.appendChild(thead);
 
@@ -47,9 +57,15 @@ const setMostrarOcultarElemento = function (esconder, elemento) {
     });
 }
 
-const removeTabelaRepetida = function (className) {
-    const tabela = document.getElementsByClassName(className);
-    while(tabela.length > 0){
-        tabela[0].remove();
-    }
+const setRemoverElementos = function(seletor) {
+    document.querySelectorAll(seletor).forEach(function(elemento) {
+        elemento.remove();
+    })
 }
+
+/*const removeTabelaRepetida = function (className) {
+    const elementClass = document.getElementsByClassName(className);
+    while(elementClass.length > 0){
+        elementClass[0].remove();
+    }
+}*/
